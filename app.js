72a373ef9de2c5a355e0ae42e30f53806d70c9c1
thekevinT <39786491+thekevinT@users.noreply.github.com>
2020-05-12 18:04:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const copyText = document.querySelector("#copyText");
+const pasteText = document.querySelector("#pasteText");
+const button = document.querySelector("button");
+const tooltip = document.querySelector(".tooltip");
+button.addEventListener('click', copyPaste);
+
+
+function copyPaste() {
+
+    copyText.select();
+    pasteText.value = "";
+    tooltip.classList.add("show");
+    setTimeout(function() {
+        tooltip.classList.remove("show");
+    }, 700);
+    if (document.execCommand("copy")) {
+        pasteText.focus();
+    } else {
+        alert("Something went wrong!");
+    }
+}
